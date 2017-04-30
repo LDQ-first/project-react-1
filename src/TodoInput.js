db39bb7ba2e87d16951cf21ba3c254281a3993a0
LDQ-first <178848890@qq.com>
2017-04-30 23:56:52 +0800

    fix value, 监听回车事件

--- src/TodoInput.js
+++ src/TodoInput.js
@@ -2,6 +2,12 @@ import React, {Component} from 'react';
 
 export default class TodoInput extends Component {
     render(){
-        return <input className="content" type="text" value={this.props.content}/>
+        return <input className="content" type="text" defaultValue={this.props.content}
+        onKeyPress={this.submit}/>  
+    }
+    submit(e){
+        if(e.key === 'Enter') {
+            console.log('用户按了回车');
+        }
     }
 }
