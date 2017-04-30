c006e838c318bf34707868bf1bb68e868f23ac4d
LDQ-first <178848890@qq.com>
2017-04-30 20:46:05 +0800

    learn react

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+/*import App from './App';*/
 import './index.css';
+import Welcome from './Welcome';
 
-/*ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);*/
+
 ReactDOM.render(
-  <h1>Hi, world!</h1>,
-  document.getElementById('root2')
-);
+  <Welcome name="Component"/>,
+  document.getElementById('root')
+)
+
+/*function tick() {
+  const element = (
+    <div>
+      <h1>Hello, React!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  ReactDOM.render(
+    element,
+    document.getElementById('root')
+  );
+}
+
+setInterval(tick, 1000);*/
