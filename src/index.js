6179a4c15e623c01ef5070422282397af8045703
LDQ-first <178848890@qq.com>
2017-04-30 21:43:09 +0800

    基本骨架完成

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-/*import App from './App';*/
+import App from './App';
 import './index.css';
-import Welcome from './Welcome';
+
 
 
 ReactDOM.render(
-  <Welcome name="Component"/>,
+  <App/>,
   document.getElementById('root')
 )
 
