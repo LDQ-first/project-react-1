8b948164ba5ab5b780867b3d032edcbbba282ab2
LDQ-first <178848890@qq.com>
2017-05-01 16:39:45 +0800

    fix value

--- src/TodoInput.js
+++ src/TodoInput.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 
 export default class TodoInput extends Component {
     render(){
-        return <input className="content" type="text" defaultValue={this.props.content}
+        return <input className="content" type="text" value={this.props.content}
         placeholder="请输入待办事项"
         onChange={this.changeTitle.bind(this)}
         onKeyPress={this.submit.bind(this)}/>
